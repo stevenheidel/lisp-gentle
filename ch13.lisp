bb87a4e66bfcacce596101d9ef108579607c0299
Steven Heidel <steven@rigetti.com>
2019-02-19 10:48:17 -0800

    It is better to remain silent and be thought a fool than to speak and remove all doubt

--- ch13.lisp
+++ ch13.lisp
@@ -19,3 +19,57 @@
              (dotimes (n count) (format t "*")))))
     (dotimes (n (length *hist-array*)) (print-line n))
     (format t "~&    ~3S total" *total-points*)))
+
+; 13.9
+
+(defvar crypto-text)
+
+(setf crypto-text
+  '("zj ze kljjls jf slapzi ezvlij pib kl jufwxuj p hffv jupi jf"
+     "enlpo pib slafml pvv bfwkj"))
+
+(defvar *encipher-table*)
+(defvar *decipher-table*)
+
+(setf *encipher-table* (make-hash-table))
+(setf *decipher-table* (make-hash-table))
+
+(defun make-substitution (old new)
+  (when (gethash new *encipher-table*)
+    (let ((existing (gethash new *encipher-table*)))
+      (error (format nil "'~A' already decodes to '~A'" existing new))))
+  (setf (gethash old *decipher-table*) new)
+  (setf (gethash new *encipher-table*) old)
+  t)
+
+(defun undo-substitution (char)
+  (let ((deciphered-char (gethash char *decipher-table*)))
+    (remhash char *decipher-table*)
+    (remhash deciphered-char *encipher-table*)))
+
+(defun clear ()
+  (clrhash *encipher-table*)
+  (clrhash *decipher-table*))
+
+(defun decipher-string (encoded-string)
+  (let* ((length (length encoded-string))
+         (result (make-string length :initial-element #\Space)))
+    (dotimes (i length result)
+      (let* ((old (aref encoded-string i))
+             (new (gethash old *decipher-table*)))
+        (when new
+          (setf (aref result i) new))))))
+
+(defun show-line (encoded-string)
+  (let ((deciphered-string (decipher-string encoded-string)))
+    (format t "~&~A" encoded-string)
+    (format t "~&~A" deciphered-string)))
+
+(defun show-text (cryptogram)
+  (dolist (string cryptogram)
+    (show-line string)
+    (format t "~%~%")))
+
+(defun solve (old new)
+  (make-substitution old new)
+  (show-text crypto-text))
